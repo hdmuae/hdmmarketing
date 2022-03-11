6ff565774a5c54ec328adb76f3ca47e5f4453f06
hdmuae <yazan@hdmuae.com>
2022-03-11 15:10:44 +0400

    feat: Portfolio projects order

--- src/containers/OurWork/data.ts
+++ src/containers/OurWork/data.ts
@@ -1,11 +1,11 @@
 export const data = [
   {
     active: false,
-    image: "/images/screen.png",
-    title: "Wind West Cafe Web Design",
-    website: "https://www.windwestcafe.com/",
+    image: "/images/screen1.png",
+    title: "Frota Academy",
+    website: "https://frota-team.ch/v2",
     description:
-      "Wind West Cafe located in Dubai needed a site that could streamline all the information to the user, making it visibly appealing but easy to use - while still maintaining it’s brand recognition. At HDM we’ve designed a simplified presentation with a holistic look and feel. By developing a Wind West Cafe website coded from scratch, with a Google integrated cafe locator, Wind West Cafe gained a more powerful tool to drive their own business, well beyond the competitors.",
+      "We were trusted by Frota Academy to help grow their digital marketing campaigns with a focus on increasing lead volume, while reducing cost-per lead. Working closely with the business owner, we developed custom, on-brand landing pages and social media ads that resulted in a dramatic increase in their weekly lead volume.",
   },
   {
     active: false,
@@ -17,10 +17,10 @@ export const data = [
   },
   {
     active: false,
-    image: "/images/screen1.png",
-    title: "Frota Academy",
-    website: "https://frota-team.ch/v2",
+    image: "/images/screen.png",
+    title: "Wind West Cafe Web Design",
+    website: "https://www.windwestcafe.com/",
     description:
-      "We were trusted by Frota Academy to help grow their digital marketing campaigns with a focus on increasing lead volume, while reducing cost-per lead. Working closely with the business owner, we developed custom, on-brand landing pages and social media ads that resulted in a dramatic increase in their weekly lead volume.",
+      "Wind West Cafe located in Dubai needed a site that could streamline all the information to the user, making it visibly appealing but easy to use - while still maintaining it’s brand recognition. At HDM we’ve designed a simplified presentation with a holistic look and feel. By developing a Wind West Cafe website coded from scratch, with a Google integrated cafe locator, Wind West Cafe gained a more powerful tool to drive their own business, well beyond the competitors.",
   },
 ];
