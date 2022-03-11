48098888631013903c3a5352c8366d00b4cc8d6e
hdmuae <yazan@hdmuae.com>
2022-03-11 16:55:10 +0400

    fix: redirects

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/contact",
+        destination: "/",
+        permanent: "false",
+      },
+      {
+        source: "/about",
+        destination: "/",
+        permanent: "false",
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
